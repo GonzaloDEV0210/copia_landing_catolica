c75ff0269c14430b6a757d6c6c9b51da1cff7125
Gonzalo Palma <gonzalopalmadev0210@gmail.com>
2024-07-22 01:15:54 -0500

    avance del proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html', './js/*.js'],
+  theme: {
+    extend: {
+      width: {
+        '106': '106px',
+        '121': '121px'
+      }
+    },
+  },
+  plugins: [],
+}
+
