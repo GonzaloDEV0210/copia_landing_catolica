885e7f4a359521fba5862130ed9f889545323064
Gonzalo Palma <gonzalopalmadev0210@gmail.com>
2024-08-09 20:51:26 -0500

    modelo mobile terminado

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
         '121': '121px',
         '360': '360px'
       },
+      height: {
+        '2px': '2px',
+      },
       boxShadow: {
         'shadow_form': '-1px 2px 14px 0px rgba(255,255,255,1)',
       },
@@ -15,6 +18,9 @@ module.exports = {
         'yellow_catolica': '#ffbf1f',
         'green_slider_catolica': 'rgba(54,162,188,.7)',
         'boton_aliados': '#36a2bc',
+      },
+      lineHeight: {
+        '18px': '18px',
       },
     },
   },
